92abb1829a0ea31ef54c410a526dd28c87b2e879
Daniel Hickey <daniellhickey@gmail.com>
2016-02-16 16:15:11 +0000

    So far the list is working just needs some styling and possibly some more behaviours added

--- list.js
+++ list.js
@@ -1,3 +1,70 @@
-/**
- * Created by hnwaterford on 16/02/2016.
+/*
+@Author: Daniel Hickey
  */
+//Self invoking function to declare variables with local scope.
+(function() {
+    var input = document.getElementById('input');
+    var btn = document.getElementById('btn');
+
+    /*List object to that holds the attributes "done"
+    and "todo" storing their respective element id's.*/
+    var lists = {
+        todo: document.getElementById('todo'),
+        done: document.getElementById('done')
+    };
+
+    /**
+     * @param {string} str
+     * @param {Function} onCheck
+     * @returns {HTMLElement}
+     */
+   function createHtml(str, onCheck) {
+        var element = document.createElement('li');
+        var checkbox = document.createElement('input');
+        var label = document.createElement('span');
+
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener('click', onCheck);
+        label.textContent = str;
+
+        element.appendChild(checkbox);
+        element.appendChild(label);
+
+        return element;
+    };
+
+    function addTask(task) {
+        lists.todo.appendChild(task);
+    };
+
+    function onCheck(event) {
+        var task = event.target.parentElement;
+        var list = task.parentElement.id;
+
+        lists[list === 'done' ? 'todo' : 'done'].appendChild(task);
+        this.checked = false;
+        input.focus();
+    };
+
+    function onInput() {
+        var str = input.value.trim();
+
+        if (str.length > 0) {
+            addTask(createHtml(str, onCheck));
+
+            input.value = '';
+            input.focus();
+        }
+    };
+
+    btn.addEventListener('click', onInput);
+    input.addEventListener('keyup', function(event) {
+        var code = event.keyCode;
+
+        if (code === 13) {
+            onInput();
+        }
+    });
+
+    input.focus();
+}());
