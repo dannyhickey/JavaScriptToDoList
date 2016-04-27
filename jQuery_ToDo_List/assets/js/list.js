f191efc851894c1ebf3f5619b88fd3cf63af5895
Daniel Hickey <daniellhickey@gmail.com>
2016-04-27 16:01:53 +0100

    Edited a custom cursor in photoshop and used it when hovering over the li elements to help the user understand items can be crossed out as well as deleted.

--- jQuery_ToDo_List/assets/js/list.js
+++ jQuery_ToDo_List/assets/js/list.js
@@ -7,15 +7,18 @@ $(".fa-plus-square-o").on("click", function(){
 	$("input[type='text']").fadeToggle(500);
 });
 
-//Click the X to delete.. will be using a fontawesome icon here later
+//Click the thrash icon to delete
 $("ul").on("click", "span", function(event)
 {
-	$(this).parent().fadeOut(500, function(){//the first "this" is refering to the <span> 
-		$(this).remove();	//and the second "this" is removing the parent <li> element
-	});
-	//stop propagation: stopping the click event from bubbling up to 
-	//the other parent elements on the page preventing triggers for other click events.
-	event.stopPropagation();
+	if(confirm("Are you sure you want to delete this item?")){
+		
+		$(this).parent().fadeOut(500, function(){//the first "this" is refering to the <span> 
+			$(this).remove();	//and the second "this" is removing the parent <li> element
+		});
+		//stop propagation: stopping the click event from bubbling up to 
+		//the other parent elements on the page preventing triggers for other click events.
+		event.stopPropagation();
+	}
 });
 
 
@@ -30,5 +33,6 @@ $("input[type='text']").keypress(function(event){
 
 	}
 });
+
 
 
